a6e0d1a21f32a642e7263f15b29ef0317ade2144
IbrahimElyyan <ibrahimelyyan17@gmail.com>
2024-02-20 13:53:20 -0500

    donwsampling done

--- release/downsample.py
+++ release/downsample.py
@@ -26,7 +26,27 @@ def main():
     # subsampling without aliasing, visualize results on 2nd row
 
     #### YOUR CODE HERE
+    im_subsample = im.copy()
+    for i in range(N_levels):
+        # Apply a Gaussian filter for anti-aliasing
+        kernel = gaussian_kernel(5, 1)
+        
+        # Separate color channels
+        r, g, b = im_subsample[:, :, 0], im_subsample[:, :, 1], im_subsample[:, :, 2]
+        
+        # Apply Gaussian filter and subsample each color channel
+        r_subsample = filter2d(r, kernel)[::2, ::2]
+        g_subsample = filter2d(g, kernel)[::2, ::2]
+        b_subsample = filter2d(b, kernel)[::2, ::2]
+        
+        # Combine color channels back into a single image
+        im_subsample = np.stack([r_subsample, g_subsample, b_subsample], axis=2)
+        
+        plt.subplot(2, N_levels, N_levels+i+1)
+        plt.imshow(im_subsample)
+        plt.axis('off')
     
+    plt.show()
     #### END YOUR CODE
     
 if __name__ == "__main__":
