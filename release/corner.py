b6beb94821c2bf27aa39aa366cbfb74c8d55f119
IbrahimElyyan <ibrahimelyyan17@gmail.com>
2024-02-20 12:43:34 -0500

    logic for harris complete

--- release/corner.py
+++ release/corner.py
@@ -21,7 +21,26 @@ def harris_corners(img, window_size=3, k=0.04):
     response = None
     
     ### YOUR CODE HERE
-
+    # Computing the x and y derivatives
+    x_der = partial_x(img)
+    y_der = partial_y(img)
+    
+    # Compute product of derivatives
+    xx_der = x_der**2
+    yy_der = y_der**2
+    xy_der = x_der*y_der
+    
+    # Compute sum of products
+    window = np.ones((window_size,window_size))
+    sum_xx = filter2d(xx_der, window)
+    sum_yy = filter2d(yy_der,window)
+    sum_xy = filter2d(xy_der,window)
+    
+    # Compute reponse using formula
+    det = xx_der*yy_der - xy_der**2
+    trace = xx_der + yy_der
+    response = det - k(trace**2)
+    
     ### END YOUR CODE
 
     return response
